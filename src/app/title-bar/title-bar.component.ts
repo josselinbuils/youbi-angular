a8bc951384cd2b19c301545cebf33133ee8d672d
Josselin BUILS <josselin.buils@gmail.com>
2018-02-14 23:12:55 +0100

    Make title bar buttons work

--- src/app/title-bar/title-bar.component.ts
+++ src/app/title-bar/title-bar.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+const remote = window.require('electron').remote;
 
 @Component({
   selector: 'app-title-bar',
   templateUrl: './title-bar.component.html',
-  styleUrls: ['./title-bar.component.scss']
+  styleUrls: ['./title-bar.component.scss'],
 })
-export class TitleBarComponent implements OnInit {
+export class TitleBarComponent {
 
-  constructor() { }
+  close() {
+    remote.getCurrentWindow().close();
+  }
 
-  ngOnInit() {
+  maximize() {
+    const window = remote.getCurrentWindow();
+
+    if (window.isMaximized()) {
+      window.unmaximize();
+    } else {
+      window.maximize();
+    }
+  }
+
+  minimize() {
+    remote.getCurrentWindow().minimize();
   }
 }
