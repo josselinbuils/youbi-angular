5b8dedc346b006a1b06407b38601503776540921
Josselin BUILS <josselin.buils@gmail.com>
2018-02-17 14:57:46 +0100

    Update project structure and add first front-end components

--- browser/app/app.module.ts
+++ browser/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserComponent } from './browser/browser.component';
 import { ControlBarComponent } from './control-bar/control-bar.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 
@@ -10,6 +11,7 @@ import { TitleBarComponent } from './title-bar/title-bar.component';
     AppComponent,
     ControlBarComponent,
     TitleBarComponent,
+    BrowserComponent,
   ],
   imports: [
     BrowserModule,
