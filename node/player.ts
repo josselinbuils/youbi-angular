1c09caade12159720c49e7ad00e868287368d0b3
Josselin BUILS <josselin.buils@gmail.com>
2018-02-22 23:38:53 +0100

    Adds pause, resume, seek and getState methods to player

--- node/player.ts
+++ node/player.ts
@@ -10,54 +10,103 @@ export class Player {
 
   private audioOutput: AudioOutput;
   private audioStream;
+  private currentMusic?: { format: DecodingFormat; durationSeconds: number };
   private decoder = Decoder.create();
-  private state: PlayerState = PlayerState.Stopped;
+  private device: { id: number };
 
   static create(): Player {
     return new Player();
   }
 
-  async play(path: string): Promise<void> {
+  getState(): PlayerState {
+    if (this.audioOutput === undefined) {
+      return PlayerState.Stopped;
+    } else if (this.audioOutput.isActive()) {
+      return PlayerState.Playing;
+    } else if (this.audioOutput.isStopped()) {
+      return PlayerState.Paused;
+    } else {
+      return PlayerState.Stopped;
+    }
+  }
+
+  pause(): PlayerState {
+    if (this.getState() === PlayerState.Playing) {
+      logger.info('Pause');
+      this.audioOutput.stop();
+    }
+    return this.getState();
+  }
+
+  async play(path: string): Promise<PlayerState> {
     logger.info(`Play ${path}`);
 
     if (!pathExistsSync(path)) {
       throw Error('File not found');
     }
 
-    // const devices = getDevices()
-    //   .map(d => `- ${d.name} (${d.hostAPIName})`)
-    //   .join('\n');
-
-    // logger.debug(`Available devices:\n${devices}\n`);
-
-    const device = getDevices()
+    this.device = getDevices()
       .filter(d => /usb|dx7/i.test(d.name) && /wdm/i.test(d.hostAPIName))[0];
 
     await this.stop();
 
-    const format = await this.decoder.getFormat(path);
-    logger.debug(`Audio format: ${format.formatID.toUpperCase()} ${format.bitsPerChannel}bit/${format.sampleRate}KHz`);
+    const format = await this.decoder.get('format', path) as DecodingFormat;
+    const durationSeconds = Math.round((await this.decoder.get('duration', path) as number) / 1000);
+    this.currentMusic = { durationSeconds, format };
 
-    this.audioOutput = this.createAudioOutput(format, device.id);
-    this.audioStream = this.decoder.start(path);
+    logger.debug(`Audio format: ${format.formatID.toUpperCase()} ${format.bitsPerChannel}bit/${format.sampleRate}Hz`);
+
+    this.audioOutput = this.createAudioOutput(format, this.device.id);
+    this.audioStream = await this.decoder.start(path);
 
     logger.debug('Links stream to audio output');
     this.audioStream.pipe(this.audioOutput);
 
     logger.debug('Starts audio output');
     this.audioOutput.start();
-    this.state = PlayerState.Playing;
+
+    setTimeout(() => {
+      this.seek(230);
+    }, 3000);
+
+    return this.getState();
   }
 
-  async stop(): Promise<void> {
+  resume(): PlayerState {
+    if (this.getState() === PlayerState.Paused) {
+      logger.info('Resume');
+      this.audioOutput.start();
+    }
+    return this.getState();
+  }
+
+  seek(timeSeconds: number): void {
+
+    if (this.currentMusic === undefined) {
+      throw new Error('No current music');
+    }
+
+    const format = this.currentMusic.format;
+    const byteOffset = timeSeconds * format.sampleRate * format.bitsPerChannel * format.channelsPerFrame / 8;
+
+    this.audioStream.unpipe(this.audioOutput);
+    this.audioOutput.close();
+    this.audioStream = this.decoder.seek(byteOffset);
+    this.audioOutput = this.createAudioOutput(format, this.device.id);
+    this.audioStream.pipe(this.audioOutput);
+    this.audioOutput.start();
+  }
+
+  stop(): PlayerState {
     if (this.decoder.isActive()) {
       logger.debug('Decoder active, stops it');
       this.decoder.stop();
     }
     if (this.audioOutput !== undefined && this.audioOutput.isActive()) {
       logger.debug('Audio output active, stops it');
-      await this.audioOutput.stop();
+      this.audioOutput.close();
     }
+    return this.getState();
   }
 
   private createAudioOutput(format: DecodingFormat, deviceId: number): AudioOutput {
@@ -67,20 +116,17 @@ export class Player {
       channelCount: format.channelsPerFrame,
       sampleFormat: format.bitsPerChannel,
       sampleRate: format.sampleRate,
-      deviceId,
+      deviceId: -1,
     });
 
-    audioOutput.on('stopped', () => {
-      logger.debug('Audio output stopped');
-      this.state = PlayerState.Stopped;
-    });
+    audioOutput.on('finish', () => logger.debug('Audio output stopped'));
 
     return audioOutput;
   }
 }
 
-enum PlayerState {
-  Paused,
-  Playing,
-  Stopped,
+export enum PlayerState {
+  Paused = 'PAUSED',
+  Playing = 'PLAYING',
+  Stopped = 'STOPPED',
 }
