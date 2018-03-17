0af7e438a3b8eab718537e88850c833034e4a194
Josselin BUILS <josselin.buils@gmail.com>
2018-03-17 22:05:57 +0100

    Adds unmaximize icon, manage focus

--- src/browser/app/title-bar/title-bar.component.ts
+++ src/browser/app/title-bar/title-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 const getCurrentElectronWindow = (window as any).getCurrentElectronWindow;
 
@@ -7,23 +7,42 @@ const getCurrentElectronWindow = (window as any).getCurrentElectronWindow;
   templateUrl: './title-bar.component.html',
   styleUrls: ['./title-bar.component.scss'],
 })
-export class TitleBarComponent {
+export class TitleBarComponent implements OnInit {
+
+  active: boolean;
+  maximized: boolean;
+
+  private window = getCurrentElectronWindow();
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    this.active = this.window.isFocused();
+    this.maximized = this.window.isMaximized();
+  }
 
   close(): void {
     getCurrentElectronWindow().close();
   }
 
   maximize(): void {
-    const window = getCurrentElectronWindow();
-
-    if (window.isMaximized()) {
-      window.unmaximize();
+    if (this.maximized) {
+      this.window.unmaximize();
     } else {
-      window.maximize();
+      this.window.maximize();
     }
   }
 
   minimize(): void {
-    getCurrentElectronWindow().minimize();
+    this.window.minimize();
+  }
+
+  ngOnInit(): void {
+    const set = (property: string, value: boolean) => {
+      this[property] = value;
+      this.changeDetectorRef.detectChanges();
+    };
+    this.window.on('blur', () => set('active', false));
+    this.window.on('focus', () => set('active', true));
+    this.window.on('maximize', () => set('maximized', true));
+    this.window.on('unmaximize', () => set('maximized', false));
   }
 }
